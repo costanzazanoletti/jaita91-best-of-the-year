0def8f7f224f7078aa6abd07799bf02073d04849
costanzazanoletti <78044829+costanzazanoletti@users.noreply.github.com>
2023-09-13 13:04:28 +0200

    link parametrico lista-dettaglio

--- src/main/java/org/learning/bestoftheyear/controller/MovieController.java
+++ src/main/java/org/learning/bestoftheyear/controller/MovieController.java
@@ -3,16 +3,19 @@ package org.learning.bestoftheyear.controller;
 import java.util.ArrayList;
 import java.util.List;
 import org.learning.bestoftheyear.model.Movie;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/movies")
 public class MovieController {
 
-  @GetMapping
+  @GetMapping // metodo che risponde a /movies
   public String movieList(Model model) {
     // recupero la lista dei movies
     List<Movie> fakeMovieList = getMovies();
@@ -28,13 +31,39 @@ public class MovieController {
     return "movies/movie-list";
   }
 
+  // metodo che risponde a /movies/id_del_movie
+  @GetMapping("/{id}") // {id} vuol dire una parte variabile del path
+  public String movieDetail(@PathVariable("id") int movieId, Model model) {
+    // nel metodo posso usare il valore di movieId che è stato letto dal path della request
+    // ricerco nella lista di movies quello con l'id passato (movieId)
+    List<Movie> movieList = getMovies();
+    Movie movieToFind = null;
+    for (Movie m : movieList) {
+      // se l'id del Movie m è uguale a quello che cerco allora ho trovato il movie
+      if (m.getId() == movieId) {
+        movieToFind = m;
+      }
+    }
+    // prima di passare il movieToFind al model, testo che non sia null
+    if (movieToFind == null) {
+      // non esiste un movie con l'id che mi hanno passato dalla request
+      // quindi tiro un'eccezione
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+          "Movie with id " + movieId + " not found");
+    }
+
+    // passo il Movie al template come attributo
+    model.addAttribute("movie", movieToFind);
+    return "movies/movie-detail";
+  }
+
   // metodo private che costruisce e ritorna una lista di Movie
   private List<Movie> getMovies() {
     List<Movie> movies = new ArrayList<>();
     // aggiungo dei movies fake di test
     movies.add(new Movie(1, "Ritorno a Java", "Pinco Pallo"));
     movies.add(new Movie(2, "Java e il Tempio Maledetto", "Severus Python"));
-    movies.add(new Movie(1, "Harry Potter e il calice di Java", "James Potter"));
+    movies.add(new Movie(3, "Harry Potter e il calice di Java", "James Potter"));
     return movies;
   }
 }
--- src/main/java/org/learning/bestoftheyear/controller/SongController.java
+++ src/main/java/org/learning/bestoftheyear/controller/SongController.java
@@ -3,10 +3,13 @@ package org.learning.bestoftheyear.controller;
 import java.util.ArrayList;
 import java.util.List;
 import org.learning.bestoftheyear.model.Song;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/songs")
@@ -21,6 +24,28 @@ public class SongController {
     // ritorno il template songs-list.html
     return "songs/songs-list";
   }
+
+  @GetMapping("/{id}")
+  public String songDetail(@PathVariable("id") int songId, Model model) {
+    // recuperlo la lista di Song
+    List<Song> songs = getSongs();
+    // cerco la Song con l'id passato nel path della request
+    Song songToFind = null;
+    for (Song s : songs) {
+      if (s.getId() == songId) {
+        songToFind = s;
+      }
+    }
+    // se la Song con quell'id non esiste sollevo un'eccezione
+    if (songToFind == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+          "Song with id " + songId + " not found");
+    }
+    // aggiungo al model l'attributo con la Song trovata
+    model.addAttribute("song", songToFind);
+    return "songs/song-detail";
+  }
+
 
   // metodo private per generare e restituire una lista di Song
   private List<Song> getSongs() {
